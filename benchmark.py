0c3c75f8f02a3fcf245b2700608720f42496b4ce
Vadim Liventsev <hello@vadim.me>
2022-03-25 19:41:08 +0100

    Baseline

--- benchmark.py
+++ benchmark.py
@@ -4,6 +4,7 @@ import psb2
 import os
 import wandb
 import random
+import pandas as pd
 
 from github import ensure_repo, upload_file
 from pbe import program_by_example
@@ -12,19 +13,30 @@ DATA_PATH = os.environ['DATA_PATH']
 HEAT_UP_RATE = 0.2
 MAX_TRIES = 1000
 
-with open('tasks.txt') as f:
+with open('psb2-meta/tasks.txt') as f:
     task_descriptions = {name.strip(): description.strip() for name, description in chunked(f.readlines(), 2)}
 
 solutions_repo = ensure_repo(os.environ['GITHUB_REMOTE'], 'solutions')
 solutions_repo.config_writer().set_value('user', 'name', os.environ['GIT_USER']).release()
 solutions_repo.config_writer().set_value('user', 'email', os.environ['GIT_EMAIL']).release()
 
+def title2kebabcase(title):
+    return '-'.join(word.lower() for word in title.split(' '))
+
+pushgp_success_rates = pd.read_csv('psb2-meta/results.tsv', sep='\t', index_col=['Problem'])['Succ.'].rename(title2kebabcase)
+
 if __name__ == '__main__':
     problems = psb2.PROBLEMS.copy()
     random.shuffle(problems)
     for problem in problems:
         try:
-            run = wandb.init(project='nl2ml-codex', config={'problem': problem, 'max_tries': MAX_TRIES, 'heat_up_rate': HEAT_UP_RATE})
+            config = {
+                'problem': problem, 
+                'baseline': pushgp_success_rates[problem],
+                'max_tries': MAX_TRIES, 
+                'heat_up_rate': HEAT_UP_RATE
+                }
+            run = wandb.init(project='nl2ml-codex', config=config)
 
             description = task_descriptions[problem]
             train_data, test_data = psb2.fetch_examples(DATA_PATH, problem, 5, 2000, format='competitive')
---
+++ psb2-meta/README.md
@@ -0,0 +1,2 @@
+This folder contains data extracted from [\[Helmuth and Kelly 2021\]](https://arxiv.org/pdf/2106.06086.pdf).
+We used [Mathpix snipping tool](https://mathpix.com/) to extract tables.
