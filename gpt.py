d98c30e8739a39e3b889267e0821d2322a496671
Vadim Liventsev <hello@vadim.me>
2022-12-14 00:06:11 +0100

    Merged gpt query fixes from angie

--- gpt.py
+++ gpt.py
@@ -3,7 +3,7 @@ from fire import Fire
 from tenacity import retry, retry_if_exception_type, stop_after_attempt
 from tenacity import wait_random_exponential, wait_fixed
 
-@retry(retry=retry_if_exception_type(openai.error.RateLimitError), 
+@retry(retry=retry_if_exception_type(openai.error.RateLimitError),
        wait=wait_random_exponential())
 @retry(retry=retry_if_exception_type(openai.error.APIError),
        wait=wait_fixed(20),
@@ -15,44 +15,43 @@ def query_gpt(code, instruction=None, n=1, temperature=1.0):
     If instruction is not specified, the code is extended (autocompleted),
     otherwise it's edited according to the instruction.
     """
+    if instruction:
+        response = openai.Edit.create(
+            engine="code-davinci-edit-001",
+            input=code,
+            n=n,
+            instruction=instruction,
+            temperature=temperature
+        )
+        result = [choice['text'] for choice in response["choices"] if "text" in choice.keys()]
+        return result if len(result) > 0 else []
+    else:
+        response = openai.Completion.create(
+            engine="code-davinci-001",
+            prompt=code,
+            n=n,
+            temperature=temperature,
+        )
+        result = [code + '\n' + choice['text'] for choice in response["choices"] if "text" in choice.keys()]
+        return result if len(result) > 0 else [code]
 
-    try:
-        if instruction:
-            response = openai.Edit.create(
-                engine="code-davinci-edit-001",
-                input=code,
-                n=n,
-                instruction=instruction,
-                temperature=temperature
-            ) 
 
-            return [choice['text'] for choice in response["choices"]]        
-        else:
-            response = openai.Completion.create(
-                engine="code-davinci-001",
-                prompt=code,
-                n=n,
-                temperature=temperature,
-                #stop=["\"\"\""]
-            )
-            
-            return [code + '\n' + choice['text'] for choice in response["choices"]]
-    except KeyError:
-        return []
-
-def explore_gpt(code, instruction=None, batch_size=1, heat_per_batch=0.2):
+def explore_gpt(code='', instruction=None, code_behaviour=None, batch_size=1, heat_per_batch=0.2):
     """Get many code snippets from GPT-3 ordered from most to least likely"""
 
     # Beam search would be preferable, but it's computationally costly
     # (for OpenAI, which is why they don't offer it)
 
-    temperature = 0
+    # We fix moderate temperature to get sufficiently varied code snippets from the model
+    temperature = 0.0
 
     while True:
         # We intentionally avoid temperature=0 
         # That would lead to a batch of identical code snippets
-        temperature += heat_per_batch
-        yield from query_gpt(code, instruction, 
+        # Update temperature but keep it 1 at max
+        temperature = temperature + heat_per_batch \
+            if 1.0 - temperature >= heat_per_batch else temperature
+        yield from query_gpt(code, instruction, code_behaviour,
                              n=batch_size, temperature=temperature)
 
 if __name__ == '__main__':
