74b5d9eb102eebc2323d2b3893046c813fd6a682
Anastasiia Grishina <angie@Anastasiias-MacBook-Pro.local>
2022-12-20 15:57:59 +0100

    java template

---
+++ code-templates/template.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+import java.applet.*;
+import java.awt.*;
+import java.net.*;
+import java.text.*;
+import java.time.*;
+import java.math.*;
+import javax.swing.*;
+
+class Solution {
+    public static void main(String args[]) {
+        /**
+        $prompt
+        */
