d9097f351ec321fb55e36b3107e8df0d80056991
Vadim Liventsev <hello@vadim.me>
2022-12-23 02:19:22 +0100

    More OpenAI screwups

--- gpt.py
+++ gpt.py
@@ -6,7 +6,8 @@ from tenacity import wait_random_exponential
        wait=wait_random_exponential())
 @retry(retry=retry_if_exception_type((openai.error.APIError, 
                                       openai.error.InvalidRequestError, 
-                                      openai.error.APIConnectionError)),
+                                      openai.error.APIConnectionError,
+                                      openai.error.ServiceUnavailableError)),
        wait=wait_random_exponential(),
        stop=stop_after_attempt(50))
 def query_gpt(code=None, code_behavior=None, instruction=None, n=1, temperature=1.0):
