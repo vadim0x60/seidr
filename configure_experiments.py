c54ec69e259c957991b7660c2f9c7aeffe4bdfcf
Anastasiia Grishina <angie@Anastasiias-MacBook-Pro.local>
2023-09-25 20:09:50 +0200

    Add parameters setup as in GECCO'23 SEIDR paper

--- configure_experiments.py
+++ configure_experiments.py
@@ -1,17 +1,25 @@
-from benchmark import run_benchmark, task_descriptions
+"""Create a separate file with a list of experiments and their Slurm task id's"""
+
 import logging
-from fire import Fire
+import pandas as pd
 import traceback
 
+from datetime import datetime
+from pathlib import Path
+
+from benchmark import task_descriptions
+
 bf_experiments = [
-    {'problem': problem, 
-     'language': language, 
-     'branching_factor': branching_factor, 
-     'max_programs': 1000, 
-     'beam_width': branching_factor}
+    {'problem': problem,
+     'language': language,
+     'branching_factor': branching_factor,
+     'max_programs': 100,
+     'beam_width': branching_factor,
+     'debug_prompt_id': 0,
+     'log': 'INFO'}
     for problem in task_descriptions.keys()
     for language in ('C++', 'Python')
-    for branching_factor in (1, 10, 100, 1000)
+    for branching_factor in (2, 4, 16)
 ]
 
 prompt_experiments = [
@@ -30,12 +38,12 @@ prompt_experiments = [
 
 experiments = bf_experiments + prompt_experiments
 
-def run_experiment_for_paper(task_id, log='ERROR'):
-    return run_benchmark(task_id=task_id, log=log, **experiments[task_id-1])
-
 if __name__ == '__main__':
     try:
-        Fire(run_experiment_for_paper)
+        timestamp = datetime.now().strftime("%d_%m_%y__%H_%M")
+        if not Path('config').exists():
+            Path('config').mkdir()
+        pd.DataFrame(experiments).to_csv(f'config/experiments_{timestamp}.csv')
     except:
         logging.error(traceback.format_exc())
         raise
